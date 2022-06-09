b993044567645a1a48cbb4d5f3af0856cdbadba9
kylian moreira <kylianmoreira@gmail.com>
2022-06-09 16:30:46 +0200

    add crud service

--- src/Form/ServiceType.php
+++ src/Form/ServiceType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Service;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,9 @@ class ServiceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
             ->add('description')
             ->add('photo')
         ;
@@ -21,7 +24,10 @@ class ServiceType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Service::class,
+            'data_class'      => Service::class,
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
+            'csrf_token_id'   => 'service_item',
         ]);
     }
 }
