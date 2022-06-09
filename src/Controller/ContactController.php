555d91e7f40b40568c140bc9393c19caa9599802
JS.Derouet <js.derouet@sfr.fr>
2022-06-09 17:07:23 +0200

    reset to the true email user to admin

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -19,6 +19,16 @@ class ContactController extends AbstractController
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = (new Email())
+                ->from($contact->getEmail())
+                ->to('ne pas mettre l email de l admin en dur')
+                ->subject('Un utilisateur vous contact')
+                ->html($this->renderView('email/email_user_to_admin.html.twig', [
+                    'contact' => $contact
+                ]));
+            $mailer->send($email);
+        }
         return $this->render('contact/index.html.twig', [
             'form' => $form->createView(),
         ]);
