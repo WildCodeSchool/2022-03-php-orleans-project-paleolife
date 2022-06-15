90d71b0be9c150612379a8597e452162c666d7f7
baloolc <viann45@hotmail.fr>
2022-06-15 12:48:12 +0200

    crud add is ok style + validator + function

---
+++ src/Entity/Session.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SessionRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SessionRepository::class)]
+class Session
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        max: 255,)]
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $name;
+
+    #[Assert\Positive]
+    #[ORM\Column(type: 'integer')]
+    private int $number;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNumber(): ?int
+    {
+        return $this->number;
+    }
+
+    public function setNumber(int $number): self
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+}
