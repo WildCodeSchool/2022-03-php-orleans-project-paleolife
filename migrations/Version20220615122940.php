23d9ab5829790af6376cb8c33b83a2637d25eb98
Alexis Thibault <alexis.thibault45150@laposte.net>
2022-06-15 14:32:05 +0200

    correction done

--- migrations/Version20220615122940.php
+++ migrations/Version20220615122940.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220615091934 extends AbstractMigration
+final class Version20220615122940 extends AbstractMigration
 {
     public function getDescription(): string
     {
