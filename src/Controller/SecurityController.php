728694a63f430afd6dac45bec13ac4efcf988542
JS.Derouet <js.derouet@sfr.fr>
2022-06-15 17:11:01 +0200

    resolve anti-slash

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,13 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Exception;
 
 class SecurityController extends AbstractController
 {
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function logout(): void
     {
-        // controller can be blank: it will never be called!
-        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+        throw new Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
