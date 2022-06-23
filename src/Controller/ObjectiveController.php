b7db935eceab7156832507ed528a431b78f656a9
kylian moreira <kylianmoreira@gmail.com>
2022-06-23 12:29:56 +0200

    secure route

--- src/Controller/ObjectiveController.php
+++ src/Controller/ObjectiveController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/objectif')]
+#[Route('/admin/objectif')]
 class ObjectiveController extends AbstractController
 {
     #[Route('/', name: 'app_objective_index', methods: ['GET'])]
