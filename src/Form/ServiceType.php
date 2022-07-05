ae5bf84ba8bec3e99d2da2a09001948c51832855
baloolc <viann45@hotmail.fr>
2022-07-05 16:13:07 +0200

    add fixture service and question

--- src/Form/ServiceType.php
+++ src/Form/ServiceType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Service;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,8 +18,9 @@ class ServiceType extends AbstractType
             ->add('title', TextType::class, [
                 'label' => 'Titre'
             ])
-            ->add('description')
-            ->add('photo', UrlType::class)
+            ->add('question', TextareaType::class, ['label' => 'Question', 'required' => false])
+            ->add('description', TextareaType::class, ['label' => 'Description'])
+            ->add('photo', UrlType::class, ['label' => 'Photo', 'required' => false])
         ;
     }
 
