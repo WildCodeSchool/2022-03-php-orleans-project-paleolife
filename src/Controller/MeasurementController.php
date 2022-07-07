0b3ef49ddb7fc5c71a13f5b075b424bec5b823b5
kylian moreira <kylianmoreira@gmail.com>
2022-07-07 11:24:37 +0200

    crud add measure

--- src/Controller/MeasurementController.php
+++ src/Controller/MeasurementController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('admin/measurement')]
+#[Route('admin/mensuration')]
 class MeasurementController extends AbstractController
 {
     #[Route('/', name: 'app_measurement_index', methods: ['GET'])]
