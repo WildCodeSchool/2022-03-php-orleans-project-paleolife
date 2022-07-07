4a0f13cf394f2134050cf798e445a1aacb9861d1
kylian moreira <kylianmoreira@gmail.com>
2022-07-07 11:54:14 +0200

    corection done

--- src/Form/MeasurementType.php
+++ src/Form/MeasurementType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Measurement;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,12 @@ class MeasurementType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('unity')
+            ->add('name', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('unity', TextType::class, [
+                'label' => 'Unité'
+            ])
         ;
     }
 
