7a17cfaafeea34f616cfb0b4a3561f252c522d7a
kylian moreira <kylianmoreira@gmail.com>
2022-07-07 10:23:17 +0200

    make crud

---
+++ src/Form/MeasurementType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Measurement;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MeasurementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('unity')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Measurement::class,
+        ]);
+    }
+}
