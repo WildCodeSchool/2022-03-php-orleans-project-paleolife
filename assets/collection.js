880ee1ff070d724f3a2f55e2ba01764797f3e931
kylian moreira <kylianmoreira@gmail.com>
2022-07-12 09:49:18 +0200

    fix style

--- assets/collection.js
+++ assets/collection.js
@@ -3,7 +3,7 @@ import a2lix_lib from '@a2lix/symfony-collection/src/a2lix_sf_collection';
 a2lix_lib.sfCollection.init({
     manageRemoveEntry: true,
     lang: {
-        add: 'Ajouter',
-        remove: 'Supprimer'
+        add: '+ Ajouter',
+        remove: '- Supprimer'
     }
 })
